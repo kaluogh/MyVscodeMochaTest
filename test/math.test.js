c498987007fb729e35cd5ee5bd2b135866092275
kaluo <zhangwenxin6615006@163.com>
2020-11-24 17:48:41 +0800

    init

---
+++ test/math.test.js
@@ -0,0 +1,9 @@
+// import { expect } from 'chai'
+// import { add } from '../math'
+
+describe('add test',  () => {
+    it('return 3 when adding  1 + 2', () => {
+        // expect(add(1,2)).to.eq(3)
+        // expect(1 + 2).to.eq(3)
+    })
+});
