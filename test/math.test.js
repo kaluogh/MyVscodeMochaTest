1f9456ae7e0bca783285e60d49bc1605601d30b0
kaluogh <zhangwenxin6615006@163.com>
2020-11-25 16:32:12 +0900

    update

--- test/math.test.js
+++ test/math.test.js
@@ -1,7 +1,6 @@
-import chai from 'chai'
-import { add } from '../math.js'
+import { expect } from 'chai'
+import { add } from '../src/math.js'
 
-const {expect} = chai
 describe('add test',  () => {
     it('return 3 when adding  1 + 2', () => {
         expect(add(1,2)).to.eq(3)
