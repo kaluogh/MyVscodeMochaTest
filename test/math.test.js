cb05b10b2449fde8dd2427aaf1c5b4ad5ec35d00
kaluogh <zhangwenxin6615006@163.com>
2020-11-26 20:40:42 +0900

    Update math.test.js

--- test/math.test.js
+++ test/math.test.js
@@ -2,7 +2,9 @@ import { expect } from 'chai'
 import { add } from '../src/math.js'
 
 describe('add test',  () => {
-    it('return 3 when adding  1 + 2', () => {
-        expect(add(1,2)).to.eq(3)
+    it('return 3 when adding  1 + 2', (done) => {
+        let tempValue = add(1,2)
+        expect(tempValue).to.eq(3)
+        done()
     })
 });
